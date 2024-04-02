7bdd7fcd9237bedd7152a12f45c6f5e3286ab3bb
salmi younes <shinigamir913@gmail.com>
2024-04-02 15:56:41 +0100

    add: vlc support

--- movie-player/Player/play.py
+++ movie-player/Player/play.py
@@ -1,31 +1,39 @@
-
+import os
 from shutil import which
 import subprocess
 
 
 
 class MpvPlayer:
-	def __init__(self) :
-		pass
-
 	def mpv_check(self) -> bool :
 		return which('mpv') is not None
 
-	def mpv_paly(self, title : str, fullsecreen : bool, stream_url : str) -> subprocess.run:
+	def mpv_paly(self, title : str, fullsecreen : bool, stream_url : str) -> subprocess.Popen:
 		if self.mpv_check() :
-			mpv = ['mpv',  "--no-terminal",  "--no-terminal"]
+			mpv = ['mpv',  "--no-terminal"]
 
 			if fullsecreen :
 				mpv.append('--fs')
 		
-			assert stream_url is not None and title is not None
+			mpv.extend([f'--force-media-title={title}', stream_url])
+			stdout = open(os.devnull, 'w')
+			return subprocess.Popen(mpv, stdout=stdout, stderr=subprocess.STDOUT)
 
-			mpv.extend([f'--force-media-title={title}', stream_url, '2>&1 dev/null' , '&'])
+		raise NoSuchPlayerFound('mpv player not found in your system .')
 
-			return subprocess.run(mpv)
+class VlcPlayer:
+	def vlc_check(self) -> bool:
+		return which('vlc') is not None
 
-		raise NoSuchPlayerFound('no media player found in your system .')
-
+	def vlc_play(self, title : str, fullsecreen : bool, stream_url : str) -> subprocess.Popen:
+		if self.vlc_check:
+			vlc = ['vlc', '--no-video-title-show']
+			if fullsecreen:
+				vlc.append('--fullscreen')
+			vlc.extend([f'--meta-title={title}', stream_url])
+			stdout = open(os.devnull, 'w')
+			return subprocess.Popen(vlc, stdout=stdout, stderr=subprocess.STDOUT)
+		raise NoSuchPlayerFound('vlc player not found in your system .')
 
 # Errors 
 class NoSuchPlayerFound(Exception):
